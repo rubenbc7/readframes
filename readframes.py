1acc5550e806c4bc9cac1fb11f8e9a726a8eb6f7
rubenbc7 <rubenborbollacastelo@gmail.com>
2021-03-12 12:14:14 -0700

    a

--- readframes.py
+++ readframes.py
@@ -1,7 +1,10 @@
 import wave
+import numpy as np
 
 GM = wave.open('good-morningMan.wav', 'r')
 
-frames = GM.readframes(-1)
 
-print(frames[:100])
+frames = GM.readframes(-1)
+ondaconvertida = np.frombuffer(frames, dtype='int16')
+
+print(frames[:10])
